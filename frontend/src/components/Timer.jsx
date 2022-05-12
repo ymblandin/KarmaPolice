e61ae682235d288e370de06f697bced57cfa0003
piment <romain@mura.io>
2022-05-12 14:54:51 +0200

    adding time functionnality and component position

--- frontend/src/components/Timer.jsx
+++ frontend/src/components/Timer.jsx
@@ -1,5 +1,14 @@
-function Timer() {
-  return <div className="Timer">Timer</div>;
+import "../assets/css/timer.css";
+
+function Timer({ timer }) {
+  const convertTime = (seconde) => {
+    return `${((seconde - (seconde % 60)) / 60).toString().padStart(2, "0")}:${(
+      seconde % 60
+    )
+      .toString()
+      .padStart(2, "0")}`;
+  };
+  return <div className="timer">{convertTime(timer)}</div>;
 }
 
 export default Timer;
