5d6165967f77c2e7730d3ffa703a4083ff698ce9
piment <romain@mura.io>
2022-05-12 17:10:50 +0200

    adding support for three.js

--- frontend/src/components/Model3D.jsx
+++ frontend/src/components/Model3D.jsx
@@ -1,9 +1,17 @@
 import "./Model3D.css";
+import { Canvas } from "@react-three/fiber";
 
 function Model3D() {
   return (
     <div className="Model3D">
-      <img src="/src/assets/savanna.jpg" alt="Savanne" />
+      {/* <img src="/src/assets/savanna.jpg" alt="Savanne" /> */}
+      <Canvas>
+        <ambientLight />
+        <mesh>
+          <boxBufferGeometry attach="geometry" />
+          <meshLambertMaterial attach="material" color="red" />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
